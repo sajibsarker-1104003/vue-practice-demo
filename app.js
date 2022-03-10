91df0b6998440cf31d9b4124ddf1c870c69d56c3
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-03-10 13:14:12 +0600

    Initialization and Instance

---
+++ app.js
@@ -0,0 +1,16 @@
+new Vue({
+  el:"#app",
+  data:{
+    title:"Hello World!",
+    arr:["Sajib",25,"Bangladeshi"],
+    obj:{
+      name:"Hello Sajib ",
+      age:25
+    },
+    fun:()=>{
+      return "gvjvjkvjhjhghghjhvjkvgjchgxdgfszfsgdjhgjf";
+    },
+    isNan:true,
+  }
+
+});
